d26be67e5fbb8ae6c62a959d755fd8857190d254
Toba-Adesanya <adesanyatoba@gmail.com>
2024-07-13 01:10:43 +0100

    Practice type conversions

--- ch2/variables.go
+++ ch2/variables.go
@@ -13,4 +13,12 @@ func main() {
 
 	// Using a function from a different package. Neat!
 	rune_func()
+
+	// Type conversions: Go enforces type casting, no implicit type conversion here!
+	var a int = 10
+	var b float64 = 90.25
+
+	var sum1 float64 = float64(a) + b
+	fmt.Printf("The sum is %.2f \n", sum1)
+
 }
