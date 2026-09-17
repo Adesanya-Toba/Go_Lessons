f5bebd47214153b534b582911b759bcc2de73b47
Toba <adesanyatoba@gmail.com>
2026-09-17 12:01:52 +0100

    variables

--- JA/ch1/main.go
+++ JA/ch1/main.go
@@ -8,7 +8,14 @@ func hello() {
 
 func main() {
 	fmt.Println("Books in stock:")
+	var title = "Alice in Wonderland"
+	var author = "Kimmy K3"
 	var book = "'Master and Commander', by Patrick O'Brian"
-
 	fmt.Println(book)
+	fmt.Println(title, "by", author)
+	book = "'A Morbid Taste for Bones', by Ellis Peters"
+	title = "A new title"
+	author = "A new author"
+	fmt.Println(book)
+	fmt.Println(title, "by", author)
 }
