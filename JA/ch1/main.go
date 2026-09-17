30f6347ce83b5dca4d75b99e513f278e91e28207
Toba <adesanyatoba@gmail.com>
2026-09-17 12:13:45 +0100

    functions

--- JA/ch1/main.go
+++ JA/ch1/main.go
@@ -6,16 +6,20 @@ func hello() {
 	fmt.Println("Hello, world!")
 }
 
+func printBook(title, author string) {
+	fmt.Println(title, "by", author)
+}
+
 func main() {
 	fmt.Println("Books in stock:")
 	var title = "Alice in Wonderland"
 	var author = "Kimmy K3"
 	var book = "'Master and Commander', by Patrick O'Brian"
 	fmt.Println(book)
-	fmt.Println(title, "by", author)
+	printBook(title, author)
 	book = "'A Morbid Taste for Bones', by Ellis Peters"
 	title = "A new title"
 	author = "A new author"
 	fmt.Println(book)
-	fmt.Println(title, "by", author)
+	printBook(title, author)
 }
